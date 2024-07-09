deba64af183db32cba646e5212df28dfc6ff575c
ieplif <fszribeiro@gmail.com>
2024-07-09 15:42:46 -0300

    Adicionando pytest.fixture Session

---
+++ tests/test_db.py
@@ -0,0 +1,20 @@
+from sqlalchemy import select
+
+from fast_zero.models import User
+
+
+def test_create_user(session):
+    user = User(
+        username='filiperibeiro',
+        email='filipe@email.com',
+        password='minhasenha',
+    )
+
+    session.add(user)
+    session.commit()
+
+    result = session.scalar(
+        select(User).where(User.email == 'filipe@email.com')
+    )
+
+    assert result.username == 'filiperibeiro'
