8028656f662c007ea369fa7f4b64bdefe09b1442
ieplif <fszribeiro@gmail.com>
2024-08-14 16:32:15 -0300

    Current_user

--- fast_zero/security.py
+++ fast_zero/security.py
@@ -1,8 +1,19 @@
 from datetime import datetime, timedelta
+from http import HTTPStatus
 
-from jwt import encode
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from jwt import decode, encode
+from jwt.exceptions import PyJWTError
 from pwdlib import PasswordHash
+from sqlalchemy import select
+from sqlalchemy.orm import Session
 from zoneinfo import ZoneInfo
+
+from fast_zero.database import get_session
+from fast_zero.models import User
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
 
 pwd_context = PasswordHash.recommended()
 
@@ -31,3 +42,26 @@ def create_access_token(data: dict):
     encoded_jwt = encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
     return encoded_jwt
+
+
+def get_current_user(
+    session: Session = Depends(get_session),
+    token: str = Depends(oauth2_scheme),
+):
+    credentials_exception = HTTPException(
+        status_code=HTTPStatus.UNAUTHORIZED,
+        detail='Could not validate credentials',
+        headers={'WWW-Authenticate': 'Bearer'},
+    )
+    try:
+        payload = decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get('sub')
+        if not username:
+            raise credentials_exception
+    except PyJWTError:
+        raise credentials_exception
+
+    user = session.scalar(select(User).where(User.email == username))
+    if not user:
+        raise credentials_exception
+    return user
--- tests/test_app.py
+++ tests/test_app.py
@@ -45,9 +45,10 @@ def test_read_users_with_user(client, user):
     assert response.json() == {'users': [user_schema]}
 
 
-def test_update_user(client, user):
+def test_update_user(client, user, token):
     response = client.put(
-        '/users/1',
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
         json={
             'password': '123',
             'username': 'testusername2',
@@ -62,8 +63,11 @@ def test_update_user(client, user):
     }
 
 
-def test_delete_user(client, user):
-    response = client.delete('/users/1')
+def test_delete_user(client, user, token):
+    response = client.delete(
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+    )
 
     assert response.json() == {'message': 'User deleted'}
 
