89d8f4d651767c0d00c6546745efe287e9927e68
ieplif <fszribeiro@gmail.com>
2024-08-15 16:04:11 -0300

    Router para Auth

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,17 +1,8 @@
 from http import HTTPStatus
 
-from fastapi import Depends, FastAPI, HTTPException
-from fastapi.security import OAuth2PasswordRequestForm
-from sqlalchemy import select
-from sqlalchemy.orm import Session
+from fastapi import FastAPI
 
-from fast_zero.database import get_session
-from fast_zero.models import User
-from fast_zero.schemas import Message, Token
-from fast_zero.security import (
-    create_access_token,
-    verify_password,
-)
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
@@ -19,20 +10,3 @@ app = FastAPI()
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
-
-
-@app.post('/token', response_model=Token)
-def login_for_access_token(
-    form_data: OAuth2PasswordRequestForm = Depends(),
-    session: Session = Depends(get_session),
-):
-    user = session.scalar(select(User).where(User.email == form_data.username))
-
-    if not user or not verify_password(form_data.password, user.password):
-        raise HTTPException(
-            status_code=400, detail='Incorrect email or password'
-        )
-
-    access_token = create_access_token(data={'sub': user.email})
-
-    return {'access_token': access_token, 'token_type': 'Bearer'}
