779d13bfa40592ba97cfdf6a940b31f09f484cc7
ieplif <fszribeiro@gmail.com>
2024-08-16 15:40:03 -0300

    Expired token

--- fast_zero/security.py
+++ fast_zero/security.py
@@ -4,7 +4,7 @@ from http import HTTPStatus
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from jwt import decode, encode
-from jwt.exceptions import PyJWTError
+from jwt.exceptions import ExpiredSignatureError, PyJWTError
 from pwdlib import PasswordHash
 from sqlalchemy import select
 from sqlalchemy.orm import Session
@@ -59,10 +59,15 @@ def get_current_user(
         username: str = payload.get('sub')
         if not username:
             raise credentials_exception
+
+    except ExpiredSignatureError:
+        raise credentials_exception
+
     except PyJWTError:
         raise credentials_exception
 
     user = session.scalar(select(User).where(User.email == username))
+
     if not user:
         raise credentials_exception
     return user
