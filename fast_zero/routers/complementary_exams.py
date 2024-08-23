070fa07c3c94d0c56a017c90ad910961f09597e0
ieplif <fszribeiro@gmail.com>
2024-08-23 13:47:05 -0300

    Create routers get and delete

--- fast_zero/routers/complementary_exams.py
+++ fast_zero/routers/complementary_exams.py
@@ -1,10 +1,17 @@
+from http import HTTPStatus
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from fast_zero.models import ComplementaryExam
-from fast_zero.schemas import ComplementaryExamsPublic, ComplementaryExamsSchema
+from fast_zero.schemas import (
+    ComplementaryExamsFilter,
+    ComplementaryExamsList,
+    ComplementaryExamsPublic,
+    ComplementaryExamsSchema,
+    Message,
+)
 from fast_zero.security import get_session
 
 router = APIRouter(prefix='/complementary_exams', tags=['complementary_exams'])
@@ -27,3 +34,34 @@ def create_complementary_exam(
     session.refresh(db_complementary_exam)
 
     return db_complementary_exam
+
+
+@router.get('/', response_model=ComplementaryExamsList)
+def list_complementary_exams(
+    session: T_Session,
+    filters: ComplementaryExamsFilter = Depends(),
+):
+    query = session.query(ComplementaryExam)
+
+    if filters.exam_details:
+        query = query.filter(ComplementaryExam.exam_details.ilike(f'%{filters.exam_details}%'))
+
+    complementary_exams = query.all()
+
+    return {'complementary_exams': complementary_exams}
+
+
+@router.delete('/{exam_id}', response_model=Message)
+def delete_complementary_exam(
+    exam_id: int,
+    session: T_Session,
+):
+    complementary_exam = session.query(ComplementaryExam).filter_by(exam_id=exam_id).first()
+
+    if not complementary_exam:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='Exam not found.')
+
+    session.delete(complementary_exam)
+    session.commit()
+
+    return {'message': 'Complementary Exam has been deleted successfully.'}
--- tests/test_complementary_exam.py
+++ tests/test_complementary_exam.py
@@ -1,3 +1,8 @@
+from http import HTTPStatus
+
+from tests.conftest import ComplementaryExamFactory, PatientFactory
+
+
 def test_create_complementary_exam(client, token):
     response = client.post(
         '/complementary_exams/',
@@ -13,3 +18,50 @@ def test_create_complementary_exam(client, token):
         'patient_id': 1,
         'exam_details': 'complementary_exam exam_details',
     }
+
+
+def test_list_complementary_exams_should_return_5_complementary_exams(session, client, token):
+    expected_complementary_exams = 5
+    session.bulk_save_objects(
+        PatientFactory.create_batch(
+            5,
+        )
+    )
+    session.bulk_save_objects(ComplementaryExamFactory.create_batch(5))
+    session.commit()
+
+    response = client.get(
+        '/complementary_exams/',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert len(response.json()['complementary_exams']) == expected_complementary_exams
+
+
+def test_list_complementary_exams_filter_exam_details_should_return_5_complementary_exams(session, client, token):
+    expected_complementary_exams = 5
+    session.bulk_save_objects(PatientFactory.create_batch(5))
+    session.bulk_save_objects(ComplementaryExamFactory.create_batch(5, exam_details='exam_details'))
+    session.commit()
+
+    response = client.get(
+        '/complementary_exams/?exam_details=exam_details',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert len(response.json()['complementary_exams']) == expected_complementary_exams
+
+
+def test_delete_complementary_exam(session, client, token):
+    complementary_exam = ComplementaryExamFactory()
+    session.add(complementary_exam)
+    session.commit()
+    session.refresh(complementary_exam)
+
+    response = client.delete(
+        f'/complementary_exams/{complementary_exam.exam_id}',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'Complementary Exam has been deleted successfully.'}
