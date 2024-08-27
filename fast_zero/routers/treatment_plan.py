979583f8f92280f6f5c8eb612f2d03c6c26f3b79
ieplif <fszribeiro@gmail.com>
2024-08-27 14:42:52 -0300

    Create tests post and get

--- fast_zero/routers/treatment_plan.py
+++ fast_zero/routers/treatment_plan.py
@@ -6,6 +6,8 @@ from sqlalchemy.orm import Session
 from fast_zero.models import TreatmentPlan
 from fast_zero.schemas import (
     PatientFilter,
+    TreatmentPlanFilter,
+    TreatmentPlanList,
     TreatmentPlanPublic,
     TreatmentPlanSchema,
 )
@@ -33,3 +35,24 @@ def create_treatment_plan(
     session.refresh(db_treatment_plan)
 
     return db_treatment_plan
+
+
+@router.get('/', response_model=TreatmentPlanList)
+def list_treatment_plans(
+    session: T_Session,
+    filters: TreatmentPlanFilter = Depends(),
+):
+    query = session.query(TreatmentPlan)
+
+    if filters.patient_id:
+        query = query.filter(TreatmentPlan.patient_id == filters.patient_id)
+    if filters.objectives:
+        query = query.filter(TreatmentPlan.objectives.ilike(f'%{filters.objectives}%'))
+    if filters.probable_sessions:
+        query = query.filter(TreatmentPlan.probable_sessions.ilike(f'%{filters.probable_sessions}%'))
+    if filters.procedures:
+        query = query.filter(TreatmentPlan.procedures.ilike(f'%{filters.procedures}%'))
+
+    treatment_plans = query.all()
+
+    return {'treatment_plans': treatment_plans}
--- tests/test_treatment_plan.py
+++ tests/test_treatment_plan.py
@@ -1,3 +1,6 @@
+from tests.conftest import TreatmentPlanFactory
+
+
 def test_create_treatment_plan(client, token):
     response = client.post(
         '/treatment-plan/',
@@ -17,3 +20,42 @@ def test_create_treatment_plan(client, token):
         'probable_sessions': 10,
         'procedures': 'treatment_plan.procedures',
     }
+
+
+def test_list_treatment_plans_should_return_5_treatment_plans(session, client, token):
+    expected_treatment_plans = 5
+    session.bulk_save_objects(TreatmentPlanFactory.create_batch(5))
+    session.commit()
+
+    response = client.get(
+        '/treatment-plan/',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert len(response.json()['treatment_plans']) == expected_treatment_plans
+
+
+def test_list_treatment_plans_filter_objectives_should_return_5_treatment_plans(session, client, token):
+    expected_treatment_plans = 5
+    session.bulk_save_objects(TreatmentPlanFactory.create_batch(5, objectives='objectives'))
+    session.commit()
+
+    response = client.get(
+        '/treatment-plan/?objectives=objectives',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert len(response.json()['treatment_plans']) == expected_treatment_plans
+
+
+def test_list_treatment_plans_filter_procedures_should_return_5_treatment_plans(session, client, token):
+    expected_treatment_plans = 5
+    session.bulk_save_objects(TreatmentPlanFactory.create_batch(5, procedures='procedures'))
+    session.commit()
+
+    response = client.get(
+        '/treatment-plan/?procedures=procedures',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert len(response.json()['treatment_plans']) == expected_treatment_plans
