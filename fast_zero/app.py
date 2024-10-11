b08fad6dbe6b8e035371d1b4eb1e7ce71b2c3e45
ieplif <fszribeiro@gmail.com>
2024-10-11 13:13:39 -0300

    New backend base

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -4,13 +4,6 @@ from fastapi import FastAPI
 
 from fast_zero.routers import (
     auth,
-    clinical_examination,
-    clinical_history,
-    complementary_exams,
-    patients,
-    physiotherapy_diagnosis,
-    prognosis,
-    treatment_plan,
     users,
 )
 from fast_zero.schemas import Message
@@ -19,13 +12,6 @@ app = FastAPI()
 
 app.include_router(users.router)
 app.include_router(auth.router)
-app.include_router(patients.router)
-app.include_router(clinical_history.router)
-app.include_router(clinical_examination.router)
-app.include_router(complementary_exams.router)
-app.include_router(physiotherapy_diagnosis.router)
-app.include_router(prognosis.router)
-app.include_router(treatment_plan.router)
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
